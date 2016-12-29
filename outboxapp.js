e7bbadc2fdd9a4b60ee10174dc375e24cad2a1ad
Unaivero <unaivero@gmail.com>
2016-12-29 19:24:18 +0100

    Add files via upload

---
+++ outboxapp.js
@@ -0,0 +1,15 @@
+var app = angular.module('OutboxApp', ['ngRoute']);
+
+ app.config(function($routeProvider) { $routeProvider
+   .when('/outbox', {
+      templateUrl: 'outboxhome.html',
+      controller: 'HomeController'
+ })
+   .when('/outbox/:id', {
+      templateUrl: 'outboxemail.html',
+      controller: 'EmailController'   
+     }) 
+   .otherwise({
+      redirect: '/outbox' 
+ });
+ });
